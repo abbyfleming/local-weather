f632ddbcfa46026d5d72d16c13f13b9bd9e3d518
Abby Moore <abbyleighanne@gmail.com>
2017-05-31 18:11:21 -0500

    update output

--- scripts/weather.js
+++ scripts/weather.js
@@ -4,7 +4,7 @@
 function oneDayWeather(zip) {
 	return new Promise(function(resolve, reject){
 		$.ajax({
-			url: `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=APIKEYHERE`
+			url: `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=c10648665493f00d731df750ee9b0436`
 		}).done(function(weatherData){
 			console.log("weatherData-->", weatherData);
 			resolve(weatherData); 
