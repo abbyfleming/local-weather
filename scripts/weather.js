73e64c31f1ab1b135a28197a6c03b11499efd111
Abby Moore <abbyleighanne@gmail.com>
2017-06-01 23:56:20 -0500

    hide yo keys

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,10 +1,16 @@
 "use strict";
 
+var config = require("./config");
+
+
 // Single day weather
 function oneDayWeather(zip) {
+	var key = config.weather;
+	console.log("key-->", key); 
+
 	return new Promise(function(resolve, reject){
 		$.ajax({
-			url: `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=c10648665493f00d731df750ee9b0436`
+			url: `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${key}`
 		}).done(function(weatherData){
 			console.log("weatherData-->", weatherData);
 			resolve(weatherData); 
