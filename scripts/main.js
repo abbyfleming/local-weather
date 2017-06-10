ef7c1c8e7b9706266fdfa7df3c8754e4fb642dd4
Abby Moore <abbyleighanne@gmail.com>
2017-06-10 17:33:36 -0500

    output shows five day

--- scripts/main.js
+++ scripts/main.js
@@ -11,42 +11,50 @@ var convertTemp = require('./helpers/convertTemp'); //Require for Browserify
 Handlebars.registerHelper('convertTemp', convertTemp); //Register Helper
 
 
-// function runWeather(){
-// 	var zipCode = $("#zipInput").val();
-// 	var isValidZip = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(zipCode);
-
-// 	if (isValidZip){
-// 		weather.oneDayWeather(zipCode)
-// 		.then((data) =>{
-// 			//output data to HTML
-// 			createHTML(data);
-// 		});
-
-// 	} else {
-// 		window.alert("Woah there, we need a valid zip code.");
-// 	}
-// }
-
-
-/* SAMPLE DATA */
 function runWeather(){
+	var zipCode = $("#zipInput").val();
+	var isValidZip = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(zipCode);
 
-	let data = {
-			"coord":{"lon":139,"lat":35},
-			"sys":{"country":"JP","sunrise":1369769524,"sunset":1369821049},
-			"weather":[{"id":804,"main":"clouds","description":"overcast clouds","icon":"04n"}],
-			"main":{"temp":289.5,"humidity":89,"pressure":1013,"temp_min":287.04,"temp_max":292.04},
-			"wind":{"speed":7.31,"deg":187.002},
-			"rain":{"3h":0},
-			"clouds":{"all":92},
-			"dt":1369824698,
-			"id":1851632,
-			"name":"Shuzenji",
-			"cod":200
-		};
 
-		createHTML(data);
+	if (isValidZip) {
+		weather.oneDayWeather(zipCode)
+		.then((data) => {
+			// do something
+			console.log("data-->", data); 
+			// createHTML(data);
+		});
+
+	} else {
+		window.alert("Woah there, we need a valid zip code.");
+	}
 }
+
+
+
+function runFive() {
+
+	let weatherData = [];
+	
+	weather.fiveDayWeather(37091)
+		.then((data) => {
+
+			// deconstruct the data being return for easy handlings
+			data.forEach(function(item) {
+
+				let daily = {
+					day: item.temp.day,
+					night: item.temp.night,
+					description: item.weather[0].main
+				};
+				
+				weatherData.push(daily);
+			});
+		
+			console.log("weatherData-->", weatherData); 
+			createHTML(weatherData);
+		});
+}
+
 
 
 function createHTML(weatherData) {
@@ -58,9 +66,10 @@ function createHTML(weatherData) {
 
 /* ----- EVENT LISTENERS ----- */
 $('#submitButton').click(function() {
-	runWeather();
-}); 
+	// runWeather();
+	// runFive();
 
+}); 
 
 
 module.exports = {runWeather, createHTML};
--- scripts/weather.js
+++ scripts/weather.js
@@ -6,16 +6,27 @@ var config = require("./config");
 // Single day weather
 function oneDayWeather(zip) {
 	var key = config.weather;
-	console.log("key-->", key); 
 
 	return new Promise(function(resolve, reject){
 		$.ajax({
 			url: `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${key}`
 		}).done(function(weatherData){
-			console.log("weatherData-->", weatherData);
 			resolve(weatherData); 
 		});
 	});
 }
 
-module.exports = {oneDayWeather}; 
+// Five day forecast
+function fiveDayWeather(zip) {
+	var key = config.weather;
+
+	return new Promise(function(resolve, reject){
+		$.ajax({
+			url:`http://api.openweathermap.org/data/2.5/forecast/daily?zip=${zip}&cnt=5&APPID=${key}`
+		}).done(function(weatherData){
+			resolve(weatherData.list);
+		});
+	});
+}
+
+module.exports = {oneDayWeather, fiveDayWeather}; 
